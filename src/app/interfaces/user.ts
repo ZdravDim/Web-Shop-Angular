f8ea3c03352ee55f8445c72de6f67d0a382e66f9
Stefan <mikicstefan1@gmail.com>
2024-07-21 14:05:06 +0200

    complete about page

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id: number;
+    firstname: string;
+    lastname: string;
+    email: string;
+    phone: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
