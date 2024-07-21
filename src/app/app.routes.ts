daadde0e97b4c2be1506aaa802cfd6c3dc397712
Katarina Dimic <dimicc.k@gmail.com>
2024-07-22 00:17:10 +0200

    add cart and profile icons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,18 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { UserGuard } from './user.guard';
+import { LoginComponent } from './pages/login-signup/login/login.component';
+import { SignupComponent } from './pages/login-signup/signup/signup.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'contact', component: ContactComponent },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'cart', component: CartComponent },
+    { path: 'profile', component: ProfileComponent, canActivate: [UserGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent }
 ];
